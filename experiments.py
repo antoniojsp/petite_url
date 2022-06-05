0c62889075198edb27d37d856ba6e2dab077e39f
Antonio Silva Paucar <antonios@uoregon.edu>
2022-06-05 12:06:34 -0700

    good

--- experiments.py
+++ experiments.py
@@ -1,8 +1,8 @@
-# import datetime
-#
-# def expiration_time()
-#     current_time = datetime.datetime.now()
-#     time_change = datetime.timedelta(minutes=10)
-#     new_time = date_and_time + time_change
-#
-#     return new_time
+import datetime
+
+def expiration_time():
+    current_time = datetime.datetime.now()
+    time_change = datetime.timedelta(minutes=10)
+    new_time = current_time + time_change
+
+    return new_time
