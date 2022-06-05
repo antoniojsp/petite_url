154c8b93e88858374e04d89fd4142f0a82e649f7
Antonio Silva Paucar <antonios@uoregon.edu>
2022-06-05 13:21:40 -0700

    good

--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, SubmitField
+from wtforms import StringField, validators, SubmitField, BooleanField
 from wtforms.validators import DataRequired, URL
 
 
 class PetiteURLForms(FlaskForm):
     url = StringField(label='URL', validators=[DataRequired()])
+    expiration = BooleanField(label='Expires')
     submit = SubmitField(label="Shorten")
