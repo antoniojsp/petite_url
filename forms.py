88c23cc7944f4b501b3025f5301490b82c07ad95
antonio Silva <antonios@uoregon.edu>
2022-06-12 08:23:33 -0700

    check that personalized is alphameric

--- forms.py
+++ forms.py
@@ -1,10 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, URL
+from wtforms import StringField, SubmitField, BooleanField, DateTimeField
+from wtforms.validators import DataRequired, Length
+from wtforms.fields import DateTimeLocalField
 
 
 class PetiteURLForms(FlaskForm):
     url = StringField(label='URL', validators=[DataRequired()])
-    expires = BooleanField(label='Do you want the link to expire?  ')
-    submit = SubmitField(label="Shorten")
-    personalized_name = BooleanField(label="Do you want to personalized the hash value?")
+    expires = BooleanField(label='Do you want the link to expire?  ', render_kw={"onclick": "hide_show_expiration('date_local', '#expires')"})
+    submit = SubmitField(label="Shorten", render_kw={"class":"btn btn-success"})
+    personalized_name = BooleanField(label="Do you want to personalized the hash value?", render_kw={"onclick": "hide_show_expiration('personalized', '#personalized_name')"})
+    custom_hash = StringField(validators=[Length(max=7)])
+    exp_date = DateTimeLocalField()
