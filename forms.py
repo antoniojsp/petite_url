4d5b4df010848129de547682d2fa970b2795fd51
antonio Silva <antonios@uoregon.edu>
2022-06-12 08:29:18 -0700

    forms

--- forms.py
+++ forms.py
@@ -5,9 +5,15 @@ from wtforms.fields import DateTimeLocalField
 
 
 class PetiteURLForms(FlaskForm):
-    url = StringField(label='URL', validators=[DataRequired()], render_kw={"class":"form-control p-2s", "placeholder":"Paste here the complete URL you want to shorten"})
-    expires = BooleanField(label='Do you want the link to expire?  ', render_kw={"onclick": "hide_show_expiration('date_local', '#expires')"})
-    submit = SubmitField(label="Shorten", render_kw={"class":"btn btn-success"})
-    personalized_name = BooleanField(label="Do you want to personalized the hash value?", render_kw={"onclick": "hide_show_expiration('personalized', '#personalized_name')"})
-    custom_hash = StringField(validators=[Length(max=7)], render_kw={"placeholder":"Custom name"})
+    url = StringField(label='URL', validators=[DataRequired()],
+                      render_kw={"class": "form-control p-2s",
+                                 "placeholder" "Paste here the complete URL you want to shorten"})
+    expires = BooleanField(label='Do you want the link to expire?  ',
+                           render_kw={"onclick": "hide_show_expiration('date_local', '#expires')"})
+    submit = SubmitField(label="Shorten",
+                         render_kw={"class": "btn btn-success"})
+    personalized_name = BooleanField(label="Do you want to personalized the hash value?",
+                                     render_kw={"onclick": "hide_show_expiration('personalized', '#personalized_name')"})
+    custom_hash = StringField(validators=[Length(max=7)],
+                              render_kw={"placeholder": "Custom name"})
     exp_date = DateTimeLocalField()
