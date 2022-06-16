ecc1e468c577a20b7468ced29ccf55596fd23c43
antonio Silva <antonios@uoregon.edu>
2022-06-15 23:42:14 -0700

    Works ok in regular side but no in mobile devices

--- static/index.js
+++ static/index.js
@@ -65,7 +65,7 @@ function is_only_alphanumeric(str) {
 // parts of responses
 var alert1 = '<div  id="response-alert" class="alert alert-success alert-dismissible fade show in text-center" role="alert">'
 var alert2 = '<button class="btn btn-outline-success btn-sm" onclick="clipboard()"> </a>'
-var alert3 = '</button> <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+var alert3 = '</button> <button type="button" id="clear_id" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
 // Triggers
 $(document).ready(function() {
     $('form').submit(function(e) {
@@ -127,7 +127,7 @@ $(document).ready(function() {
                       result = data.result.response;
                       if (is_url_valid(result)){
                         $("#response").html(alert1 +'<div> The shorten URL is ' + '<a id="petite_url" href="'
-                        + result + '  "Target="_blank">' + result + '      </a>       ' + alert2 + '   Copy PetiteURL!</div>' + alert3 );
+                        + result + '" Target="_blank"> ' + result + '</a>' + alert2 + 'Copy!</div>' + alert3 );
                       }else{
                         $("#response").html(alert1 + result + alert3);
                       }
