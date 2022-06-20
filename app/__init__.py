3997a135a5c10ace2585b46d956eea41cba547f4
antonio Silva <antonios@uoregon.edu>
2022-06-19 19:49:49 -0700

    add variables at __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from app.database import PetiteUrlDatabase
+from app.config import Config
 import os
 
 db = PetiteUrlDatabase(os.environ['URI'])
+my_info = Config().dict()
--- app/routes/views.py
+++ app/routes/views.py
@@ -1,21 +1,18 @@
-from flask import redirect, render_template, request, Blueprint
 from app.forms import PetiteURLForms
-from app.database import PetiteUrlDatabase
-import os
-from app.config import Config
 from app import db
-# db = PetiteUrlDatabase(os.environ['URI'])
+from app import my_info
+from flask import redirect, render_template, request, Blueprint
 
 ruta = Blueprint('routes', __name__)
-my_info = Config().dict()
 
-# @ruta.route('/', methods=["GET", "POST"])
+
+@ruta.route('/', methods=["GET", "POST"])
 def index():
     index_form = PetiteURLForms()
     return render_template("index.html", form=index_form, info=my_info), 200
 
 
-# @ruta.route('/<shorten_url_hash>')
+@ruta.route('/<shorten_url_hash>')
 def redirect_from_token(shorten_url_hash: str):
     query_answer = db.query_url(shorten_url_hash)
     result = f'{request.url_root}{shorten_url_hash}'
@@ -27,6 +24,6 @@ def redirect_from_token(shorten_url_hash: str):
     # PetiteURL.logger.info(f'Redirecting to ->  result')
     return redirect(query_answer, code=302)
 
-
-ruta.add_url_rule('/', view_func=index, methods=["GET", "POST"])
-ruta.add_url_rule('/<shorten_url_hash>', view_func=redirect_from_token)
+#
+# ruta.add_url_rule('/', view_func=index, methods=["GET", "POST"])
+# ruta.add_url_rule('/<shorten_url_hash>', view_func=redirect_from_token)
